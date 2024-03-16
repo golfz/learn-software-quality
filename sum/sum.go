51d69a3cb9a3b3d5fc6e334a8499c53ecd51addd
surattikorn <surattikorn@gmail.com>
2024-03-16 15:32:36 +0700

    feat: implement Sum

--- sum/sum.go
+++ sum/sum.go
@@ -1,1 +1,5 @@
 package sum
+
+func Sum(a, b int) int {
+	return a + b
+}
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,5 +3,12 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Log("testing sum")
+	t.Run("Test 1+2=3", func(t *testing.T) {
+		want := 3
+		got := Sum(1, 2)
+		if got != want {
+			t.Errorf("want %d, got %d", want, got)
+		}
+	})
+
 }
